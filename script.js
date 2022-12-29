29930f18899b91e496d77fa0f1114ebd496a216c
LauraMamian <lauramamianc@unicauca.edu.co>
2022-12-29 18:07:50 -0500

    clase 04 completada

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-const btn = document.querySelector("[data-form-btn]");
+(() => {
 
-// Arrow function o funciones anónimas
+    const btn = document.querySelector("[data-form-btn]");
 
-const createTask = (e) => {
-    e.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card");
-    input.value = "";
-    const content = `
-    <div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>
-    `;
-    task.innerHTML = content;
-    list.appendChild(task);
+    // Arrow function
 
-    console.log(content);
-}
+    const createTask = (e) => {
+        e.preventDefault();
+        const input = document.querySelector("[data-form-input]");
+        const value = input.value;
+        const list = document.querySelector("[data-list]");
+        const task = document.createElement("li");
+        task.classList.add("card");
+        input.value = "";
 
-btn.addEventListener("click", createTask);
+        const taskContent = document.createElement("div");
+        taskContent.appendChild(checkComplete());
+        const titleTast = document.createElement("span");
+        titleTast.classList.add("task");
+        titleTast.innerText = value;
+
+        taskContent.appendChild(titleTast);
+
+        const content = `<i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+        task.appendChild(taskContent);
+        list.appendChild(task);
+    }
+
+    btn.addEventListener("click", createTask);
+
+    const checkComplete = () => {
+        const i = document.createElement("i");
+        i.classList.add("far", "fa-check-square", "icon");
+        i.addEventListener("click", completeTask);
+        return i;
+    }
+
+    // Immediately Invoked Function Expression IIFE
+
+    const completeTask = (e) => {
+        const element = e.target;
+        element.classList.toggle("fas");
+        element.classList.toggle("completeIcon");
+        element.classList.toggle("far");
+    }
+})();
